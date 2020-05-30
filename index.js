cfab54724fa0982fa3add0e7c903866c95a4bcf5
Makinde Adeagbo <makinde@adeagbo.com>
2020-05-29 17:01:05 -0700

    Big Upgrade
    - Gracefully handle server shutdowns (using terminus)
    - Don't worry about forcing SSL, do that at the edge
    - Remove option for an init script (use `node -r` instead)

--- index.js
+++ index.js
@@ -1,59 +1,46 @@
 require('engine-strict').check(); // Check node version ASAP
 require('dotenv').load(); // and get the environment set up
 
-// Determine where to get the express app and initialization script from
-// Do this before any includes so that the init script has a chance to
-// instrument the includes if it wants
 const path = require('path');
 const program = require('commander');
-
-const currentDir = process.cwd();
-program
-  .option('-i, --init <initScript>', 'Initialization script')
-  .option('--app <appScript>', 'Script to provide the app to serve', '');
-program.parse(process.argv);
-
-if (program.init) {
-  const initPath = path.join(currentDir, program.init);
-  require(initPath); // eslint-disable-line
-}
-
-const compression = require('compression');
 const helmet = require('helmet');
 const logger = require('morgan');
-const expressEnforcesSSL = require('express-enforces-ssl');
 const express = require('express');
 const http = require('http');
+const { createTerminus } = require('@godaddy/terminus');
+
+// Determine where to get the express app script from
+const currentDir = process.cwd();
+program
+  .option('--app <appScript>', 'Script to provide the app to serve', '');
+program.parse(process.argv);
 
 const appPath = path.join(currentDir, program.app);
 const appPromise = require(appPath); // eslint-disable-line
 
-const isProd = process.env.NODE_ENV === 'production';
 const port = parseInt(process.env.PORT, 10) || 4000;
 
 // Try to resolve to app, in case it returned a promise of an app instead of
 // an actual express app. This is useful in the dev startup process for
 // next apps. See `app.prepare()` here:
 // https://www.npmjs.com/package/next#custom-server-and-routing
-Promise.resolve(appPromise).then((hostedApp) => {
+Promise.resolve(appPromise).then(({
+  app: hostedApp, onShutdown = () => {},
+}) => {
   const masterApp = express();
-  masterApp.use(
-    helmet(),
-    logger('dev'),
-    compression(),
-  );
-  if (isProd) {
-    masterApp.enable('trust proxy');
-    masterApp.use(expressEnforcesSSL());
-  }
+  masterApp.use(helmet());
+  masterApp.use(logger('dev'));
   masterApp.use(hostedApp);
 
   const server = http.createServer(masterApp);
+  createTerminus(server, {
+    timeout: 20000,
+    signals: ['SIGTERM', 'SIGINT', 'uncaughtException', 'unhandledRejection'],
+    onShutdown,
+  });
 
-  /**
- * Event listener for HTTP server "error" event.
- */
-  function onError(error) {
+  server.listen(port);
+  server.on('error', (error) => {
     if (error.syscall !== 'listen') {
       throw error;
     }
@@ -75,25 +62,12 @@ Promise.resolve(appPromise).then((hostedApp) => {
       default:
         throw error;
     }
-  }
-
-  /**
- * Event listener for HTTP server "listening" event.
- */
-  function onListening() {
+  });
+  server.on('listening', () => {
     const addr = server.address();
     const bind = typeof addr === 'string'
       ? `pipe ${addr}`
       : `port ${addr.port}`;
     console.info(`Listening on ${bind}`);
-  }
-
-  /**
- * Listen on provided port, on all network interfaces.
- */
-  server.listen(port, () => {
-    if (process.send) process.send('online'); // for browser refresh
   });
-  server.on('error', onError);
-  server.on('listening', onListening);
 });
